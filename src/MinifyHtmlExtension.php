17718da7e62bca14a0931eb0ddb6f0df0c194f1c
Nicolas Lemoine <nico.lemoine@gmail.com>
2016-08-28 16:25:19 +0200

    Initial commit

---
+++ src/MinifyHtmlExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Bolt\Extension\Hellonico\MinifyHtml;
+
+use Bolt\Extension\SimpleExtension;
+use Bolt\Controller\Zone;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use WyriHaximus\HtmlCompress\Factory;
+
+class MinifyHtmlExtension extends SimpleExtension
+{
+
+    private $version = 'v0.1.0';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function subscribe(EventDispatcherInterface $dispatcher)
+    {
+        $dispatcher->addListener(KernelEvents::RESPONSE, function (FilterResponseEvent $event) {
+
+            $response = $event->getResponse();
+            $request = $event->getRequest();
+
+            $app = $this->getContainer();
+
+            if ( !Zone::isFrontend($request) || $app['config']->get('general/debug') ) {
+                return;
+            }
+
+            $parser = Factory::construct();
+            $response->setContent($parser->compress($response->getContent()));
+
+        });
+    }
+}
