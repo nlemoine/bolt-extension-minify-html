b1c7a5ac9d3031666f53e1590e53854b071f7d29
Nicolas Lemoine <nico.lemoine@gmail.com>
2016-12-14 17:24:24 +0100

    bolt-extension-minify-html 0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Minify HTML extension for Bolt CMS
+
+Minify your HTML output.
+
+## Usage
+
+No config needed. Minification is disabled if `debug` is set to `true`.
---
+++ src/MinifyHtmlExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bolt\Extension\Hellonico\MinifyHtml;
+
+$autoload = __DIR__ . '/../vendor/autoload.php';
+if (is_file($autoload)) {
+    require $autoload;
+}
+
+use Bolt\Extension\SimpleExtension;
+use Bolt\Controller\Zone;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use WyriHaximus\HtmlCompress\Factory;
+
+class MinifyHtmlExtension extends SimpleExtension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function subscribe(EventDispatcherInterface $dispatcher)
+    {
+        $dispatcher->addListener(KernelEvents::RESPONSE, function (FilterResponseEvent $event) {
+            $response = $event->getResponse();
+            $request = $event->getRequest();
+
+            $app = $this->getContainer();
+
+            if (!Zone::isFrontend($request) || $app['config']->get('general/debug')) {
+                return;
+            }
+
+            if (!class_exists('WyriHaximus\HtmlCompress\Factory')) {
+                return;
+            }
+
+            $parser = Factory::construct();
+            $response->setContent($parser->compress($response->getContent()));
+        });
+    }
+}
